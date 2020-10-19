096d1bb9da5a12b927f47b60da477259de43ad4f
mehtasan <sanjeet.mehta@embold.io>
2020-10-19 19:28:09 +0530

    Added pipeline

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -16,14 +16,37 @@ def call(body) {
 
     pipeline {
         agent any
+        tools {
+            maven 'Maven 3.3.9'
+            jdk 'jdk8'
+        }
         stages {
             stage('Git Checkout') {
                 steps {
                     checkout([
-        $class: 'GitSCM',
-        branches: [[name:  args.branch ]],
-        userRemoteConfigs: [[ url: args.url ]]
-    ])
+                     $class: 'GitSCM',
+                        branches: [[name:  args.branch ]],
+                        userRemoteConfigs: [[ url: args.url ]]
+                    ])
+                }
+            }
+            stage ('Initialize') {
+                steps {
+                    sh '''
+                    echo "PATH = ${PATH}"
+                    echo "M2_HOME = ${M2_HOME}"
+                '''
+                }
+            }
+
+            stage ('Build') {
+                steps {
+                    sh 'mvn -Dmaven.test.failure.ignore=true install'
+                }
+                post {
+                    success {
+                        junit 'target/surefire-reports/**/*.xml'
+                    }
                 }
             }
         }
