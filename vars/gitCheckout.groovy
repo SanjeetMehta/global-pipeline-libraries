99c0bf01849da2be7b5acc3406d84ae036e7536a
mehtasan <sanjeet.mehta@embold.io>
2020-10-19 19:19:32 +0530

    Added pipeline

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,9 +1,29 @@
 #!/usr/bin/env groovy
 
-def call(Map stageParams) {
-    checkout([
+def call(body) {
+    def args = [
+        // general arguments
+        branch: '',
+        url:''
+    ]
+
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = args
+    body()
+    echo "INFO: ${args.serviceName}"
+
+    pipeline {
+        agent any
+        stages {
+            stage('Git Checkout') {
+                steps {
+                    checkout([
         $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
+        branches: [[name:  args.branch ]],
+        userRemoteConfigs: [[ url: args.url ]]
     ])
-  }
+                }
+            }
+        }
+    }
+}
