8894c4574dec08d7510082d3e1c8df656fca2e82
mehtasan <sanjeet.mehta@embold.io>
2020-10-19 18:14:15 +0530

    renamed library

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,9 +1,18 @@
 #!/usr/bin/env groovy
 
-def call(Map stageParams) {
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
-  }
+def call(body) {
+    def args = [
+        // general arguments
+        serviceName: ''
+    ]
+
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = args
+    body()
+    echo "INFO: ${args.serviceName}"
+    pipeline {
+        triggers {
+            pollSCM("H/5 6-20 * * 1-5")
+        }
+    }
+}
